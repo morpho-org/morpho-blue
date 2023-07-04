fef155cdba4b70aa3bee17aa159c9fccbd83cea7
MathisGD <mathis@morpho.xyz>
2023-07-04 11:16:07 +0200

    chore: settle solidity pragma

--- src/libraries/SafeTransferLib.sol
+++ src/libraries/SafeTransferLib.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.0;
+pragma solidity ^0.8.0;
 
 import {IERC20} from "src/interfaces/IERC20.sol";
 
--- test/forge/Market.t.sol
+++ test/forge/Market.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity 0.8.20;
 
 import {IERC20} from "src/interfaces/IERC20.sol";
 import {IOracle} from "src/interfaces/IOracle.sol";
