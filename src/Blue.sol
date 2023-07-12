39718a5118d010fbeac2eba23662bac30d46a0b2
MerlinEgalite <egalite.merlin@gmail.com>
2023-07-12 20:27:26 +0200

    refactor: extract constants

--- src/Blue.sol
+++ src/Blue.sol
@@ -6,27 +6,16 @@ import {IERC20} from "src/interfaces/IERC20.sol";
 import {IOracle} from "src/interfaces/IOracle.sol";
 
 import {MathLib} from "src/libraries/MathLib.sol";
+import {
+    WAD,
+    ALPHA,
+    EIP712_MSG_PREFIX,
+    EIP712_NAME,
+    EIP712_DOMAIN_TYPEHASH,
+    EIP712_AUTHORIZATION_TYPEHASH,
+    MAX_VALID_ECDSA_S
+} from "src/libraries/Constants.sol";
 import {SafeTransferLib} from "src/libraries/SafeTransferLib.sol";
-
-uint256 constant WAD = 1e18;
-uint256 constant ALPHA = 0.5e18;
-
-/// @dev The prefix used for EIP-712 signature.
-string constant EIP712_MSG_PREFIX = "\x19\x01";
-
-/// @dev The name used for EIP-712 signature.
-string constant EIP712_NAME = "Blue";
-
-/// @dev The domain typehash used for the EIP-712 signature.
-bytes32 constant EIP712_DOMAIN_TYPEHASH =
-    keccak256("EIP712Domain(string name,uint256 chainId,address verifyingContract)");
-
-/// @dev The typehash for approveManagerWithSig Authorization used for the EIP-712 signature.
-bytes32 constant EIP712_AUTHORIZATION_TYPEHASH =
-    keccak256("Authorization(address delegator,address manager,bool isAllowed,uint256 nonce,uint256 deadline)");
-
-/// @dev The highest valid value for s in an ECDSA signature pair (0 < s < secp256k1n ÷ 2 + 1).
-uint256 constant MAX_VALID_ECDSA_S = 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0;
 
 // Market id.
 type Id is bytes32;
