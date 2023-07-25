e54f7b0e3bd6588fd5adfad41d9fe0b8e9bfef6b
Rubilmax <rmilon@gmail.com>
2023-07-25 11:50:59 +0200

    refactor(libraries): use solmate in mocks

--- src/libraries/SafeTransferLib.sol
+++ src/libraries/SafeTransferLib.sol
@@ -66,33 +66,4 @@ library SafeTransferLib {
 
         require(success, "TRANSFER_FAILED");
     }
-
-    function safeApprove(IERC20 token, address to, uint256 amount) internal {
-        bool success;
-
-        /// @solidity memory-safe-assembly
-        assembly {
-            // Get a pointer to some free memory.
-            let freeMemoryPointer := mload(0x40)
-
-            // Write the abi-encoded calldata into memory, beginning with the function selector.
-            mstore(freeMemoryPointer, 0x095ea7b300000000000000000000000000000000000000000000000000000000)
-            mstore(add(freeMemoryPointer, 4), and(to, 0xffffffffffffffffffffffffffffffffffffffff)) // Append and mask the "to" argument.
-            mstore(add(freeMemoryPointer, 36), amount) // Append the "amount" argument. Masking not required as it's a full 32 byte type.
-
-            success :=
-                and(
-                    // Set success to whether the call reverted, if not we check it either
-                    // returned exactly 1 (can't just be non-zero data), or had no return data.
-                    or(and(eq(mload(0), 1), gt(returndatasize(), 31)), iszero(returndatasize())),
-                    // We use 68 because the length of our calldata totals up like so: 4 + 32 * 2.
-                    // We use 0 and 32 to copy up to 32 bytes of return data into the scratch space.
-                    // Counterintuitively, this call must be positioned second to the or() call in the
-                    // surrounding and() call or else returndatasize() will be zero during the computation.
-                    call(gas(), token, 0, freeMemoryPointer, 68, 0, 32)
-                )
-        }
-
-        require(success, "APPROVE_FAILED");
-    }
 }
--- src/mocks/FlashBorrowerMock.sol
+++ src/mocks/FlashBorrowerMock.sol
@@ -1,14 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import {IERC20} from "src/interfaces/IERC20.sol";
 import {IERC3156FlashLender} from "src/interfaces/IERC3156FlashLender.sol";
 import {IERC3156FlashBorrower, FLASH_BORROWER_SUCCESS_HASH} from "src/interfaces/IERC3156FlashBorrower.sol";
 
-import {SafeTransferLib} from "src/libraries/SafeTransferLib.sol";
+import {ERC20, SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
 
 contract FlashBorrowerMock is IERC3156FlashBorrower {
-    using SafeTransferLib for IERC20;
+    using SafeTransferLib for ERC20;
 
     IERC3156FlashLender private immutable _LENDER;
 
@@ -25,7 +24,7 @@ contract FlashBorrowerMock is IERC3156FlashBorrower {
     {
         require(msg.sender == address(_LENDER), "invalid lender");
 
-        IERC20(token).safeApprove(address(_LENDER), amount + fee);
+        ERC20(token).safeApprove(address(_LENDER), amount + fee);
 
         return FLASH_BORROWER_SUCCESS_HASH;
     }
