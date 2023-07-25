f36fdf095abd7f74d96f6d6d4b9a1ac6daff5138
Romain Milon <rmilon@gmail.com>
2023-07-25 09:56:18 +0200

    docs(flash-loan): modify natspecs
    Co-authored-by: MathisGD <74971347+MathisGD@users.noreply.github.com>
    Signed-off-by: Romain Milon <rmilon@gmail.com>

--- src/interfaces/IFlashBorrower.sol
+++ src/interfaces/IFlashBorrower.sol
@@ -13,7 +13,7 @@ interface IFlashBorrower {
     /// @param initiator The initiator of the loan.
     /// @param token The loan currency.
     /// @param amount The amount of tokens lent.
-    /// @param data Arbitrary data structure, intended to contain user-defined parameters.
+    /// @param data Arbitrary data, intended to contain user-defined parameters.
     /// @return The keccak256 hash of "FlashBorrower.onFlashLoan" and any additional arbitrary data.
     function onFlashLoan(address initiator, IERC20 token, uint256 amount, bytes calldata data)
         external
