47b5ffdfda03d1b0d14a5a2410745eb86fae5a92
Romain Milon <rmilon@gmail.com>
2023-07-25 11:04:29 +0200

    docs(flash-lender): update natspecs
    Co-authored-by: Merlin Egalite <44097430+MerlinEgalite@users.noreply.github.com>
    Signed-off-by: Romain Milon <rmilon@gmail.com>

--- src/interfaces/IFlashLender.sol
+++ src/interfaces/IFlashLender.sol
@@ -9,7 +9,7 @@ import {IFlashBorrower} from "./IFlashBorrower.sol";
 interface IFlashLender {
     /// @dev Initiates a flash loan.
     /// @param receiver The receiver of the tokens in the loan, and the receiver of the callback.
-    /// @param token The loan currency.
+    /// @param token The token lent.
     /// @param amount The amount of tokens lent.
     /// @param data Arbitrary data structure, intended to contain user-defined parameters.
     function flashLoan(IFlashBorrower receiver, IERC20 token, uint256 amount, bytes calldata data)
