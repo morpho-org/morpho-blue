5b064830bbf760ed7dd06be5931c55d502ea3061
MerlinEgalite <egalite.merlin@gmail.com>
2023-07-26 21:46:03 +0200

    chore: solc 0.8.21

--- src/Blue.sol
+++ src/Blue.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.20;
+pragma solidity 0.8.21;
 
 import {IIrm} from "src/interfaces/IIrm.sol";
 import {IERC20} from "src/interfaces/IERC20.sol";
