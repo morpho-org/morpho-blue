3b2105652c8a752739665d84b9232d66dbe32044
Rubilmax <rmilon@gmail.com>
2023-07-26 11:59:58 +0200

    refactor(pragma): use 0.8.21

--- src/Blue.sol
+++ src/Blue.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.20;
+pragma solidity 0.8.21;
 
 import {IBlue} from "./interfaces/IBlue.sol";
 import {IIrm} from "./interfaces/IIrm.sol";
