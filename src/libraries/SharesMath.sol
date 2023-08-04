fc1fa9ef805ffe03d3a5124747ca8c0155d6997f
Rubilmax <rmilon@gmail.com>
2023-08-04 15:30:06 +0200

    feat(shares-math): add util to get shares from amount

--- src/libraries/SharesMath.sol
+++ src/libraries/SharesMath.sol
@@ -34,4 +34,36 @@ library SharesMath {
     function toAssetsUp(uint256 shares, uint256 totalAssets, uint256 totalShares) internal pure returns (uint256) {
         return shares.mulDivUp(totalAssets + VIRTUAL_ASSETS, totalShares + VIRTUAL_SHARES);
     }
+
+    /// @dev Calculates the amount of shares corresponding to a given amount of supply.
+    /// Note: only works as long as totalSupplyShares >= totalSupply.
+    function toSupplyShares(uint256 amount, uint256 totalSupply, uint256 totalSupplyShares)
+        internal
+        pure
+        returns (uint256)
+    {
+        uint256 shares = toSharesUp(amount, totalSupply, totalSupplyShares);
+
+        if (amount != toAssetsDown(shares, totalSupply, totalSupplyShares)) {
+            return toSharesDown(amount + 1, totalSupply, totalSupplyShares);
+        }
+
+        return shares;
+    }
+
+    /// @dev Calculates the amount of shares corresponding to a given amount of borrow.
+    /// Note: only works as long as totalBorrowShares >= totalBorrow.
+    function toBorrowShares(uint256 amount, uint256 totalBorrow, uint256 totalBorrowShares)
+        internal
+        pure
+        returns (uint256)
+    {
+        uint256 shares = toSharesDown(amount, totalBorrow, totalBorrowShares);
+
+        if (amount != 0 && amount != toAssetsUp(shares, totalBorrow, totalBorrowShares)) {
+            return toSharesUp(amount - 1, totalBorrow, totalBorrowShares);
+        }
+
+        return shares;
+    }
 }
