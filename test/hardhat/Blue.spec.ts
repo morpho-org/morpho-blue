359393a6a57503eb95afbbbea8adfd6b7b3a05da
julien <julien@morpho.xyz>
2023-08-08 17:37:52 +0200

    chore: run `yarn lint`

--- test/hardhat/Blue.spec.ts
+++ test/hardhat/Blue.spec.ts
@@ -136,7 +136,14 @@ describe("Blue", () => {
         const totalSupplyShares = await blue.totalSupplyShares(id);
         Promise.all([
           blue.connect(user).supply(market, amount, user.address, []),
-          blue.connect(user).withdraw(market, amount.mul(totalSupplyShares.add(BigNumber.WAD)).div(totalSupply.add(1)).div(2), user.address, user.address),
+          blue
+            .connect(user)
+            .withdraw(
+              market,
+              amount.mul(totalSupplyShares.add(BigNumber.WAD)).div(totalSupply.add(1)).div(2),
+              user.address,
+              user.address,
+            ),
         ]);
       } else {
         const totalSupply = await blue.totalSupply(id);
@@ -150,7 +157,14 @@ describe("Blue", () => {
           Promise.all([
             blue.connect(user).supplyCollateral(market, amount, user.address, []),
             blue.connect(user).borrow(market, amount.div(2), user.address, user.address),
-            blue.connect(user).repay(market, amount.mul(totalBorrowShares.add(BigNumber.WAD)).div(totalBorrow.add(1)).div(4), user.address, []),
+            blue
+              .connect(user)
+              .repay(
+                market,
+                amount.mul(totalBorrowShares.add(BigNumber.WAD)).div(totalBorrow.add(1)).div(4),
+                user.address,
+                [],
+              ),
             blue.connect(user).withdrawCollateral(market, amount.div(8), user.address, user.address),
           ]);
         }
