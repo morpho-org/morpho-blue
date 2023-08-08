9c4479b159ba795277136ab47c376a335343a820
Jean-Grimal <jeanbgrimal@gmail.com>
2023-08-08 10:31:33 +0200

    fix: senders addresses

--- test/forge/BlueInvariantBase.t.sol
+++ test/forge/BlueInvariantBase.t.sol
@@ -16,14 +16,14 @@ contract InvariantBaseTest is BlueBaseTest {
     }
 
     function _targetDefaultSenders() internal {
-        targetSender(0x1000000000000000000000000000000000000000);
-        targetSender(0x0100000000000000000000000000000000000000);
-        targetSender(0x0010000000000000000000000000000000000000);
-        targetSender(0x0001000000000000000000000000000000000000);
-        targetSender(0x0000100000000000000000000000000000000000);
-        targetSender(0x0000010000000000000000000000000000000000);
-        targetSender(0x0000001000000000000000000000000000000000);
-        targetSender(0x0000000100000000000000000000000000000000);
+        targetSender(address(uint160(uint256(keccak256("Morpho Blue address1")))));
+        targetSender(address(uint160(uint256(keccak256("Morpho Blue address2")))));
+        targetSender(address(uint160(uint256(keccak256("Morpho Blue address3")))));
+        targetSender(address(uint160(uint256(keccak256("Morpho Blue address4")))));
+        targetSender(address(uint160(uint256(keccak256("Morpho Blue address5")))));
+        targetSender(address(uint160(uint256(keccak256("Morpho Blue address6")))));
+        targetSender(address(uint160(uint256(keccak256("Morpho Blue address7")))));
+        targetSender(address(uint160(uint256(keccak256("Morpho Blue address8")))));
     }
 
     function _weightSelector(bytes4 selector, uint256 weight) internal {
