5bbbe776df755b4ac33c646fbf769ce075c124a0
Romain Milon <rmilon@gmail.com>
2023-08-09 15:22:15 +0200

    docs(oracle): update comment
    Signed-off-by: Romain Milon <rmilon@gmail.com>

--- src/interfaces/IOracle.sol
+++ src/interfaces/IOracle.sol
@@ -2,6 +2,6 @@
 pragma solidity >=0.5.0;
 
 interface IOracle {
-    /// @notice Returns the price of the collateral asset quoted in the borrowable asset and its unit scale.
+    /// @notice Returns the price of the collateral asset quoted in the borrowable asset and the price's unit scale.
     function price() external view returns (uint256 collateralPrice, uint256 scale);
 }
