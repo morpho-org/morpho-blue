5f0a829e1fed271a171b9a3ce7a2bc169533ff9a
Patrick Kim <pakim249@ischool.berkeley.edu>
2023-08-09 14:29:40 +0200

    test: fix comparison and implement suggestion

--- test/forge/Math.t.sol
+++ test/forge/Math.t.sol
@@ -17,7 +17,7 @@ contract MathTest is Test {
         assertLe(result, toCompare, "rate should be less than the compounded rate");
         assertGe(
             result,
-            FixedPointMathLib.WAD + timeElapsed * rate / FixedPointMathLib.WAD,
+            FixedPointMathLib.WAD + timeElapsed * rate,
             "rate should be greater than the simple interest rate"
         );
         assertLe((toCompare - result) * 100_00 / toCompare, 8_00, "The error should be less than or equal to 8%");
@@ -25,14 +25,10 @@ contract MathTest is Test {
 
     // Exponentiation by squaring with rounding up.
     function wPow(uint256 x, uint256 n) private pure returns (uint256 z) {
-        z = n % 2 != 0 ? x : FixedPointMathLib.WAD;
-
-        for (n /= 2; n != 0; n /= 2) {
+        z = FixedPointMathLib.WAD;
+        for (; n != 0; n /= 2) {
+            z = n % 2 != 0 ? z.mulWadUp(x) : z;
             x = x.mulWadUp(x);
-
-            if (n % 2 != 0) {
-                z = z.mulWadUp(x);
-            }
         }
     }
 }
