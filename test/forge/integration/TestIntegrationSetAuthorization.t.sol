7fb1bcee9f22fbf73241ed7853d96bd8c1c5da53
makcandrov <makcandrov@proton.me>
2023-08-16 04:42:00 -0700

    fix: replace `_bound` with `bound`

--- test/forge/integration/TestIntegrationSetAuthorization.t.sol
+++ test/forge/integration/TestIntegrationSetAuthorization.t.sol
@@ -43,7 +43,7 @@ contract IntegrationAuthorization is BaseTest {
     }
 
     function testAuthorizationWithSigWrongPK(Authorization memory authorization, uint256 privateKey) public {
-        authorization.deadline = _bound(authorization.deadline, block.timestamp + 1, type(uint256).max);
+        authorization.deadline = bound(authorization.deadline, block.timestamp + 1, type(uint256).max);
 
         // Private key must be less than the secp256k1 curve order.
         privateKey = bound(privateKey, 1, type(uint32).max);
@@ -58,8 +58,8 @@ contract IntegrationAuthorization is BaseTest {
     }
 
     function testAuthorizationWithSigWrongNonce(Authorization memory authorization, uint256 privateKey) public {
-        authorization.deadline = _bound(authorization.deadline, block.timestamp + 1, type(uint256).max);
-        authorization.nonce = _bound(authorization.nonce, 1, type(uint256).max);
+        authorization.deadline = bound(authorization.deadline, block.timestamp + 1, type(uint256).max);
+        authorization.nonce = bound(authorization.nonce, 1, type(uint256).max);
 
         vm.assume(authorization.nonce != 0);
 
@@ -76,7 +76,7 @@ contract IntegrationAuthorization is BaseTest {
     }
 
     function testAuthorizationWithSig(Authorization memory authorization, uint256 privateKey) public {
-        authorization.deadline = _bound(authorization.deadline, block.timestamp + 1, type(uint256).max);
+        authorization.deadline = bound(authorization.deadline, block.timestamp + 1, type(uint256).max);
 
         // Private key must be less than the secp256k1 curve order.
         privateKey = bound(privateKey, 1, type(uint32).max);
