5599e4309a77ac7fc72cfdf58da24cc0466eb199
Quentin Garchery <garchery.quentin@gmail.com>
2023-08-17 11:53:47 +0200

    refactor: remove interface and code length check

--- src/libraries/SafeTransferLib.sol
+++ src/libraries/SafeTransferLib.sol
@@ -5,6 +5,11 @@ import {ErrorsLib} from "../libraries/ErrorsLib.sol";
 
 import {IERC20} from "../interfaces/IERC20.sol";
 
+interface ERC20 {
+    function transfer(address to, uint256 value) external returns (bool);
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+}
+
 /// @title SafeTransferLib
 /// @author Morpho Labs
 /// @custom:contact security@morpho.xyz
@@ -12,19 +17,13 @@ import {IERC20} from "../interfaces/IERC20.sol";
 ///         not returning a boolean for `transfer` and `transferFrom` functions.
 library SafeTransferLib {
     function safeTransfer(IERC20 token, address to, uint256 value) internal {
-        (bool success, bytes memory returndata) = address(token).call(abi.encodeCall(token.transfer, (to, value)));
-        require(
-            success && address(token).code.length > 0 && (returndata.length == 0 || abi.decode(returndata, (bool))),
-            ErrorsLib.TRANSFER_FAILED
-        );
+        (bool success, bytes memory returndata) = address(token).call(abi.encodeCall(ERC20.transfer, (to, value)));
+        require(success && (returndata.length == 0 || abi.decode(returndata, (bool))), ErrorsLib.TRANSFER_FAILED);
     }
 
     function safeTransferFrom(IERC20 token, address from, address to, uint256 value) internal {
         (bool success, bytes memory returndata) =
-            address(token).call(abi.encodeCall(token.transferFrom, (from, to, value)));
-        require(
-            success && address(token).code.length > 0 && (returndata.length == 0 || abi.decode(returndata, (bool))),
-            ErrorsLib.TRANSFER_FROM_FAILED
-        );
+            address(token).call(abi.encodeCall(ERC20.transferFrom, (from, to, value)));
+        require(success && (returndata.length == 0 || abi.decode(returndata, (bool))), ErrorsLib.TRANSFER_FROM_FAILED);
     }
 }
