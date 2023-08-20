2901c3666c5d057e903b02075ff373196a05cd5e
MathisGD <mathis@morpho.xyz>
2023-08-20 12:03:28 +0200

    test: utils lib

--- test/forge/UtilsLib.t.sol
+++ test/forge/UtilsLib.t.sol
@@ -8,6 +8,21 @@ import "src/libraries/UtilsLib.sol";
 contract UtilsLibTest is Test {
     using UtilsLib for uint256;
 
+    function testExactlyOneZeroTrue(uint256 x, uint256 y) public {
+        vm.assume((x > 0 && y == 0) || (x == 0 && y > 0));
+        assertTrue(UtilsLib.exactlyOneZero(x, y));
+    }
+
+    function testExactlyOneZeroFalse(uint256 x, uint256 y) public {
+        vm.assume((x == 0 && y == 0) || (x > 0 && y > 0));
+        assertFalse(UtilsLib.exactlyOneZero(x, y));
+    }
+
+    function testMin(uint256 x, uint256 y) public {
+        uint256 expectedMin = x < y ? x : y;
+        assertEq(UtilsLib.min(x, y), expectedMin);
+    }
+
     function testToUint128(uint256 x) public {
         vm.assume(x <= type(uint128).max);
         assertEq(uint256(x.toUint128()), x);
