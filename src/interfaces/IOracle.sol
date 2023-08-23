ffaafdde03300b7d387330f6471649fe97c0e631
Rubilmax <rmilon@gmail.com>
2023-08-23 15:09:19 +0200

    refactor(oracle): update natspecs

--- src/interfaces/IOracle.sol
+++ src/interfaces/IOracle.sol
@@ -7,8 +7,8 @@ pragma solidity >=0.5.0;
 /// @notice Interface that oracles used by Morpho must implement.
 interface IOracle {
     /// @notice Returns the price of 1 asset of collateral token quoted in 1 asset of borrowable token, scaled by 1e36.
-    /// @dev It corresponds to the price of 10^(collateral decimals) assets of collateral token quoted in
-    /// 10^(borrowable decimals) assets of borrowable token with `36 + collateral decimals - borrowable decimals`
+    /// @dev It corresponds to the price of 10**(collateral decimals) assets of collateral token quoted in
+    /// 10**(borrowable decimals) assets of borrowable token with `36 + borrowable decimals - collateral decimals`
     /// decimals of precision.
     function price() external view returns (uint256);
 }
