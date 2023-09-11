fa020767fb41a27a63918c5288d70a2c2ed799d6
Quentin Garchery <garchery.quentin@gmail.com>
2023-09-11 17:58:37 +0200

    refactor: remove unnecessary storage variables in tokens

--- certora/dispatch/ERC20USDT.sol
+++ certora/dispatch/ERC20USDT.sol
@@ -4,18 +4,12 @@ pragma solidity ^0.8.0;
 contract ERC20USDT {
     uint256 public constant MAX_UINT = 2 ** 256 - 1;
 
-    string public name;
-    string public symbol;
-    uint256 public decimals;
     uint256 public totalSupply;
     address public owner;
     mapping(address => uint256) public balanceOf;
-    mapping(address => mapping(address => uint256)) public allowed;
+    mapping(address => mapping(address => uint256)) public allowance;
 
-    constructor(string memory _name, string memory _symbol, uint256 _decimals) {
-        name = _name;
-        symbol = _symbol;
-        decimals = _decimals;
+    constructor() {
         owner = msg.sender;
     }
 
@@ -34,20 +28,16 @@ contract ERC20USDT {
     }
 
     function transferFrom(address _from, address _to, uint256 _amount) public {
-        if (allowed[_from][msg.sender] < MAX_UINT) {
-            allowed[_from][msg.sender] -= _amount;
+        if (allowance[_from][msg.sender] < MAX_UINT) {
+            allowance[_from][msg.sender] -= _amount;
         }
         _transfer(_from, _to, _amount);
     }
 
     function approve(address _spender, uint256 _amount) public {
-        require(!((_amount != 0) && (allowed[msg.sender][_spender] != 0)));
+        require(!((_amount != 0) && (allowance[msg.sender][_spender] != 0)));
 
-        allowed[msg.sender][_spender] = _amount;
-    }
-
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
-        return allowed[_owner][_spender];
+        allowance[msg.sender][_spender] = _amount;
     }
 
     function mint(address _receiver, uint256 _amount) public onlyOwner {
