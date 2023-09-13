3b180c4b727571530b96ae597014c1cb1f82caa7
Quentin Garchery <garchery.quentin@gmail.com>
2023-09-13 12:10:57 +0200

    chore: increase timeout on split to 12 sec

--- certora/scripts/verifyExactMath.sh
+++ certora/scripts/verifyExactMath.sh
@@ -8,6 +8,6 @@ certoraRun \
     certora/harness/MorphoHarness.sol \
     src/mocks/OracleMock.sol \
     --verify MorphoHarness:certora/specs/ExactMath.spec \
-    --prover_args '-smt_hashingScheme plaininjectivity' \
+    --prover_args '-smt_hashingScheme plaininjectivity -mediumTimeout 12' \
     --msg "Morpho Blue Exact Math" \
     "$@"
