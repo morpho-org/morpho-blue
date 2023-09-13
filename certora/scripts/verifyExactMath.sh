5a611ffa68e86c6635c1554780024bbe96bf0aeb
Quentin Garchery <garchery.quentin@gmail.com>
2023-09-13 15:12:13 +0200

    refactor: remove unused oracle in Certora script

--- certora/scripts/verifyExactMath.sh
+++ certora/scripts/verifyExactMath.sh
@@ -6,7 +6,6 @@ make -C certora munged
 
 certoraRun \
     certora/harness/MorphoHarness.sol \
-    src/mocks/OracleMock.sol \
     --verify MorphoHarness:certora/specs/ExactMath.spec \
     --prover_args '-smt_hashingScheme plaininjectivity -mediumTimeout 12' \
     --msg "Morpho Blue Exact Math" \
