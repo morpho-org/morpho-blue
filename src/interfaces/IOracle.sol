f42820fa42ebd56553abe66f50c4bf8d66a3397e
MathisGD <mathis@morpho.xyz>
2023-09-18 09:35:25 +0200

    docs: minor improvement

--- src/interfaces/IOracle.sol
+++ src/interfaces/IOracle.sol
@@ -7,8 +7,8 @@ pragma solidity >=0.6.2;
 /// @notice Interface that oracles used by Morpho must implement.
 interface IOracle {
     /// @notice Returns the price of 1 asset of collateral token quoted in 1 asset of loan token, scaled by 1e36.
-    /// @dev It corresponds to the price of 10**(collateral decimals) assets of collateral token quoted in
-    /// 10**(loan decimals) assets of loan token with `36 + loan decimals - collateral decimals`
+    /// @dev It corresponds to the price of 10**(collateral token decimals) assets of collateral token quoted in
+    /// 10**(loan token decimals) assets of loan token with `36 + loan token decimals - collateral token decimals`
     /// decimals of precision.
     function price() external view returns (uint256);
 }
