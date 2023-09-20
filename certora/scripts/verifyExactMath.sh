b8680aeba206f1998666f1525d8082d0545f5314
ALEX JOSEPH <alexjoseph@ALEXs-MacBook-Pro.local>
2023-09-20 08:32:38 +0100

    increase medium timeout to 30

--- certora/scripts/verifyExactMath.sh
+++ certora/scripts/verifyExactMath.sh
@@ -5,6 +5,6 @@ set -euxo pipefail
 certoraRun \
     certora/harness/MorphoHarness.sol \
     --verify MorphoHarness:certora/specs/ExactMath.spec \
-    --prover_args '-smt_hashingScheme plaininjectivity -mediumTimeout 12' \
+    --prover_args '-smt_hashingScheme plaininjectivity -mediumTimeout 30' \
     --msg "Morpho Blue Exact Math" \
     "$@"
