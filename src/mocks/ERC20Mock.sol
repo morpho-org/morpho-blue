563066238307b9f5d04c711614bc674149a48bb1
Rubilmax <rmilon@gmail.com>
2023-09-26 11:14:28 +0200

    refactor(mocks): simplify erc20

--- src/mocks/ERC20Mock.sol
+++ src/mocks/ERC20Mock.sol
@@ -4,16 +4,16 @@ pragma solidity ^0.8.0;
 import {IERC20} from "./interfaces/IERC20.sol";
 
 contract ERC20Mock is IERC20 {
-    uint256 public constant decimals = 18;
-
     uint256 public totalSupply;
 
     mapping(address account => uint256) public balanceOf;
     mapping(address account => mapping(address spender => uint256)) public allowance;
 
     function setBalance(address account, uint256 amount) public virtual {
-        _burn(account, balanceOf[account]);
-        _mint(account, amount);
+        if (amount > balanceOf[account]) totalSupply += amount - balanceOf[account];
+        else totalSupply -= balanceOf[account] - amount;
+
+        balanceOf[account] = amount;
     }
 
     function approve(address spender, uint256 amount) public virtual returns (bool) {
@@ -48,19 +48,5 @@ contract ERC20Mock is IERC20 {
         emit Transfer(from, to, amount);
 
         return true;
-    }
-
-    function _mint(address to, uint256 amount) internal virtual {
-        totalSupply += amount;
-        balanceOf[to] += amount;
-
-        emit Transfer(address(0), to, amount);
-    }
-
-    function _burn(address from, uint256 amount) internal virtual {
-        balanceOf[from] -= amount;
-        totalSupply -= amount;
-
-        emit Transfer(from, address(0), amount);
     }
 }
