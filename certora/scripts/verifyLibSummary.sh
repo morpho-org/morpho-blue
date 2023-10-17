473e750e55ce3516f56a2b180df3caad84a98c01
Quentin Garchery <garchery.quentin@gmail.com>
2023-10-17 13:18:06 +0200

    feat: add every script as a configuration file

--- certora/scripts/verifyLibSummary.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euxo pipefail
-
-certoraRun \
-    certora/harness/MorphoHarness.sol \
-    --verify MorphoHarness:certora/specs/LibSummary.spec \
-    --msg "Morpho Blue Lib Summary" \
-    "$@"
--- certora/scripts/verifyLiveness.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euxo pipefail
-
-certoraRun \
-    certora/harness/MorphoInternalAccess.sol \
-    --verify MorphoInternalAccess:certora/specs/Liveness.spec \
-    --msg "Morpho Blue Liveness" \
-    "$@"
