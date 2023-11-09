ef599aca206030d3e7f8495609594b4d089bba30
MathisGD <74971347+MathisGD@users.noreply.github.com>
2023-11-09 13:20:40 +0100

    fix: flashLoan returnData
    Signed-off-by: MathisGD <74971347+MathisGD@users.noreply.github.com>

--- src/Morpho.sol
+++ src/Morpho.sol
@@ -427,6 +427,8 @@ contract Morpho is IMorphoStaticTyping {
         bytes memory returnData = IMorphoFlashLoanCallback(msg.sender).onMorphoFlashLoan(assets, data);
 
         IERC20(token).safeTransferFrom(msg.sender, address(this), assets);
+        
+        return returnData;
     }
 
     /* AUTHORIZATION */
