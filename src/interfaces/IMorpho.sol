e5a5cb415b6c91aa8fd24ee97f67b08342cdff67
MathisGD <74971347+MathisGD@users.noreply.github.com>
2023-11-13 09:40:47 +0100

    docs: minor improvements
    Co-authored-by: Romain Milon <rmilon@gmail.com>
    Signed-off-by: MathisGD <74971347+MathisGD@users.noreply.github.com>

--- src/interfaces/IMorpho.sol
+++ src/interfaces/IMorpho.sol
@@ -130,7 +130,7 @@ interface IMorphoBase {
     /// @dev Either `assets` or `shares` should be zero. Most usecases should rely on `assets` as an input so the caller
     /// is guaranteed to have `assets` tokens pulled from their balance, but the possibility to mint a specific amount
     /// of shares is given for full compatibility and precision.
-    /// @dev Upon reset of a market (withdrawing all supply shares), the supply share price suddenly decreases.
+/// @dev When withdrawing all supply shares, the supply share price instantly resets to `VIRTUAL_ASSETS`:`VIRTUAL_SHARES`.
     /// @dev Supplying a large amount can revert for overflow.
     /// @param marketParams The market to supply assets to.
     /// @param assets The amount of assets to supply.
@@ -172,7 +172,7 @@ interface IMorphoBase {
     /// @dev Either `assets` or `shares` should be zero. Most usecases should rely on `assets` as an input so the caller
     /// is guaranteed to borrow `assets` of tokens, but the possibility to mint a specific amount of shares is given for
     /// full compatibility and precision.
-    /// @dev Upon reset of a market (repaying all borrow shares), the borrow share price suddenly decreases.
+    /// @dev When repaying all borrow shares, the borrow share price instantly resets to `VIRTUAL_ASSETS`:`VIRTUAL_SHARES`.
     /// @dev `msg.sender` must be authorized to manage `onBehalf`'s positions.
     /// @dev Borrowing a large amount can revert for overflow.
     /// @param marketParams The market to borrow assets from.
