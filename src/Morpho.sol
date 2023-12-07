faef77c2905a28f4b641a6d26a1f04ed7491e44a
Jean-Grimal <83286814+Jean-Grimal@users.noreply.github.com>
2023-12-07 14:12:31 +0100

    Update src/Morpho.sol
    Co-authored-by: Romain Milon <rmilon@gmail.com>
    Signed-off-by: Jean-Grimal <83286814+Jean-Grimal@users.noreply.github.com>

--- src/Morpho.sol
+++ src/Morpho.sol
@@ -159,7 +159,7 @@ contract Morpho is IMorphoStaticTyping {
 
         emit EventsLib.CreateMarket(id, marketParams);
 
-        // Call IRM in case it is stateful.
+        // Call to initialize IRM in case it is stateful.
         IIrm(marketParams.irm).borrowRate(marketParams, market[id]);
     }
 
